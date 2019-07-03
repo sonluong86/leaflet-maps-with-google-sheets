94f47900f07e7956311554753b9f18beea6d3e49
sonluong86 <46946303+sonluong86@users.noreply.github.com>
2019-07-03 22:27:14 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_XJ3bt_e9ZF2vcqyzGp88SJjFKzy_yPLElh_ae92jzQ/edit#gid=0';
